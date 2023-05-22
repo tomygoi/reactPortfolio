8a84e8b50508f2f6c3b240cb9addee0a08cf417f
Tony Trinh <tonytrinh911@gmail.com>
2023-05-21 23:01:32 -0400

    cleared project cards and added image to asset folder

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 function Header() {
   return (
-    <div className="h-screen flex items-start">
+    <div className="h-screen flex items-start" id="home">
       <section className="w-full bg-cover bg-center py-8 md:py-16">
         <div className="container max-w-lg mx-auto text-center text-[#0A100D]">
           <h1 className="text-5xl font-medium mb-6">Tony Trinh</h1>
-          <p className="text-xl mb-12">
+          <p className="text-xl mb-12 md:text-lg md:mb-8 lg:text-xl">
             My name is Tony Trinh and I am a University of Georgia Alumni! I am
             attending this boot camp to broaden my experience in computer
             science and eventually seek work in this field. I previously
@@ -14,19 +14,19 @@ function Header() {
           </p>
           <nav className="flex space-x-4 justify-center">
           <a
-            href="#"
+            href="#home"
             className="bg-[#902923] text-white py-4 px-12 rounded-full hover:bg-[#A22C29]"
           >
             Home
           </a>
           <a
-            href="#"
+            href="#projects"
             className="bg-[#902923] text-white py-4 px-12 rounded-full hover:bg-[#A22C29]"
           >
             Projects
           </a>
           <a
-            href="#"
+            href="#contact"
             className="bg-[#902923] text-white py-4 px-12 rounded-full hover:bg-[#A22C29]"
           >
             Contact
